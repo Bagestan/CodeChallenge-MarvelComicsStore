6094f6009bbbb2d5ffe1798a05ba233debc0ee83
Andre Bagestan <andrebagestan@gmail.com>
2023-11-28 16:15:41 -0300

    Marvel Api Service + Api Type

---
+++ src/app/services/marvel-api.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import * as Md5 from 'js-md5';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { ComicDataWrapper } from './comic-types';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MarvelApiService {
+  privateKey = '6d53fc3ff7d280903674317614830ed311cc6257';
+  publicKey = '6ba597247f7744ba170cc1eec992f833';
+  apiUrl = 'https://gateway.marvel.com/v1/public/';
+
+  private allComics = new BehaviorSubject<ComicDataWrapper>(
+    {} as ComicDataWrapper
+  );
+  private allComics$: Observable<ComicDataWrapper> =
+    this.allComics.asObservable();
+
+  constructor(private http: HttpClient) {
+    const url = this.createURL('comics');
+    this.http.get(url).subscribe((data) => {
+      console.log('🚀 ~ data:', data);
+      this.allComics.next(data as ComicDataWrapper);
+    });
+  }
+
+  private createURL(path: string) {
+    const ts = Date.now();
+
+    const params = new URLSearchParams({
+      ts: ts.toString(),
+      apikey: this.publicKey,
+      hash: Md5.md5(ts + this.privateKey + this.publicKey),
+    }).toString();
+
+    const endpoint = `${this.apiUrl}${path}?`;
+
+    return endpoint + params;
+  }
+
+  getComics(): Observable<ComicDataWrapper> {
+    return this.allComics$;
+  }
+}
